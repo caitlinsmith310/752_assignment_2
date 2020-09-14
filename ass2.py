775da37dd195c2d5e23a1f6b1021d085c89f839a
caitlinsmith310 <53849077+caitlinsmith310@users.noreply.github.com>
2020-09-14 19:25:17 +1200

    Add files via upload

--- ass2.py
+++ ass2.py
@@ -8,23 +8,7 @@ Created on Sun Sep 13 14:54:54 2020
 import numpy as np
 import math as math
 import matplotlib as plt
-from scipy.optimize import fsolve
-import numpy as np
 import matplotlib.pyplot as plt
-import os
-from uncertainties import ufloat
-from uncertainties import unumpy
-import scipy.linalg as linalg
-from scipy.optimize import curve_fit
-from matplotlib.patches import Polygon
-from scipy.integrate import quad
-import scipy.integrate as integrate
-import scipy.special as special
-from scipy.integrate import quad
-import matplotlib
-matplotlib.axes.Axes.errorbar
-matplotlib.pyplot.errorbar
-
 
 def op_gain(A,N,N_t, ep, P):
     G=A*(N-N_t)*(1-ep*P)
@@ -47,7 +31,6 @@ q=1.6e-19 #c
 N_t=2e18 #cm**-3
 
 P_l=40*1e-12  #pulse length
-
 dt=P_l/10000 #s
 t=np.arange(0,10*P_l,dt)
 I_b=10e-3
@@ -57,16 +40,8 @@ N_array=np.zeros(len(t))
 P_array=np.zeros(len(t))
 I_array=np.zeros(len(t))
 
-P_0=1
-N_0=T_c*((I_b/(q*V))-(P_0/T_p))
-print(op_gain(A, N_0, N_t, ep, P_0), N_0)
-
-
 N=2.5e18
 P=6.94e14
-T=0  #total time, 
-
-
 
 for i in range(0, len(t)):    
     time=t[i]
@@ -82,7 +57,6 @@ for i in range(0, len(t)):
     I_array[i]=I
     P_array[i]=P
     N_array[i]=N
-    T=T+dt
 
 fig, ax1 = plt.subplots()
 color = 'tab:blue'
@@ -91,12 +65,12 @@ ax1.set_ylabel('Intensity, mA', color=color)
 ax1.plot(t*1e9, I_array*1000, color=color, linestyle="dashed")
 plt.title("Photon Density, pulse length="+str(round(P_l*1e12))+"ps")
 ax1.tick_params(axis='y', labelcolor=color)
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2 = ax1.twinx() 
 color = 'tab:red'
-ax2.set_ylabel(r'Photon Density ($\times 10^{15}cm^{-3}$)', color=color)  # we already handled the x-label with ax1
+ax2.set_ylabel(r'Photon Density ($\times 10^{15}cm^{-3}$)', color=color)
 ax2.plot(t*1e9, P_array*1e-15, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.tight_layout() 
 plt.ylim([-0.2,3])
 plt.show()
 
@@ -107,38 +81,27 @@ ax1.set_ylabel('Intensity, mA', color=color)
 ax1.plot(t*1e9, I_array*1000, color=color, linestyle="dashed")
 plt.title("Carrier Density, pulse length="+str(round(P_l*1e12))+"ps")
 ax1.tick_params(axis='y', labelcolor=color)
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2 = ax1.twinx()
 color = 'tab:red'
-ax2.set_ylabel(r'Carrier Density, ($\times 10^{18}cm^{-3}$)', color=color)  # we already handled the x-label with ax1
+ax2.set_ylabel(r'Carrier Density, ($\times 10^{18}cm^{-3}$)', color=color)  
 ax2.plot(t*1e9, N_array*1e-18, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.tight_layout()
 plt.ylim([2.49,2.67])
 plt.show()
 
-#%%
+#%% All in one plot
 
 P_l=1000*1e-12  #pulse length
-
 dt=P_l/10000 #s
 t=np.arange(0,8.5*P_l,dt)
-I_b=10e-3
-I_p=50e-3
-    
+
 N_array=np.zeros(len(t))
 P_array=np.zeros(len(t))
 I_array=np.zeros(len(t))
 
-P_0=1
-N_0=T_c*((I_b/(q*V))-(P_0/T_p))
-print(op_gain(A, N_0, N_t, ep, P_0), N_0)
-
-
 N=2.5e18
 P=6.94e14
-T=0  #total time, 
-
-
 
 for i in range(0, len(t)):    
     time=t[i]
@@ -153,18 +116,9 @@ for i in range(0, len(t)):
     G=op_gain(A, N, N_t, ep, P)
     N=N+dt*rate_N(I, q, V, N, T_c, G)    
     P=P+dt*rate_P(G, P, T_p)
-    print(P)
     I_array[i]=I
     P_array[i]=P
     N_array[i]=N
-    T=T+dt
-
-plt.figure(1)     
-#plt.plot(t,N_array, label="Photon density")
-plt.plot(t,N_array, label="Carrier density")
-#plt.plot(t, I_array, label="Intensity")
-plt.legend()
-
 
 fig, ax1 = plt.subplots()
 color = 'tab:blue'
@@ -173,12 +127,12 @@ ax1.set_ylabel('Intensity, mA', color=color)
 ax1.plot(t*1e9, I_array*1000, color=color, linestyle="dashed")
 plt.title("Photon Density")
 ax1.tick_params(axis='y', labelcolor=color)
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2 = ax1.twinx()  
 color = 'tab:red'
 ax2.set_ylabel(r'Photon Density ($\times 10^{15}cm^{-3}$)', color=color)  # we already handled the x-label with ax1
 ax2.plot(t*1e9, P_array*1e-15, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.tight_layout() 
 plt.ylim([-0.2,3])
 plt.show()
 
@@ -189,11 +143,11 @@ ax1.set_ylabel('Intensity, mA', color=color)
 ax1.plot(t*1e9, I_array*1000, color=color, linestyle="dashed")
 plt.title("Carrier Density")
 ax1.tick_params(axis='y', labelcolor=color)
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2 = ax1.twinx()  
 color = 'tab:red'
-ax2.set_ylabel(r'Carrier Density, ($\times 10^{18}cm^{-3}$)', color=color)  # we already handled the x-label with ax1
+ax2.set_ylabel(r'Carrier Density, ($\times 10^{18}cm^{-3}$)', color=color)  
 ax2.plot(t*1e9, N_array*1e-18, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.tight_layout()  
 plt.ylim([2.49,2.67])
 plt.show()
